0cc74ad6a40a47812c9a7ee4dbe8639c79843010
jordimall <jordi.mallafre@hotmail.com>
2023-12-01 21:26:27 +0100

    Finalización ejercicio 3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ejercicio1 } from "./Ejercicio1/ejercicio1";
 import { ejercicio2 } from "./Ejercicio2/ejercicio2";
+import { ejercicio3 } from "./Ejercicio3/ejercicio3";
 
 
 console.log('Ejercicio 1 \n');
@@ -7,3 +8,6 @@ ejercicio1();
 
 console.log('\n Ejercicio 2 \n');
 ejercicio2();
+
+console.log('\n Ejercicio 3 \n');
+ejercicio3();
